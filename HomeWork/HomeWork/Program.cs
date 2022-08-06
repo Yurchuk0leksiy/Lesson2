fab60646fd4970fd2f201c0378bad333afcbb053
Yurchuk 0leksiy <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>
2022-08-06 13:38:54 +0300

    MainCommit

---
+++ HomeWork/HomeWork/Program.cs
@@ -0,0 +1,61 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//int intNumber = 5;
+//double doubleNumber = 6.25;
+//bool boolTrye = true;
+//string stringName = "Happy";
+//byte Id = 1;
+//short shortNumber = 2;
+//long longNumber = 100000;
+//ulong ulongNumber = 100000000;
+
+//Console.WriteLine("string - " + stringName );
+//Console.WriteLine("bool - "  + boolTrye );
+//Console.WriteLine("string - " + stringName );
+//Console.WriteLine("byte - " + Id );
+//Console.WriteLine("ulong - " + ulongNumber );
+
+
+
+//-6 * x ^ 3 + 5 * x ^ 2 - 10 * x + 15
+//int x = -6 * x ^ 3 + 5 * x ^ 2 - 10 * x + 15;
+//int num = -6;
+
+//double xx = -6 * Math.Pow(num, 3) + 5 * Math.Pow(num, 2) - 10 * num + 15;
+
+//abs(x) * sin(x)
+//int x = 1;
+//int y = 2;
+
+//double xx = Math.Abs(x) * Math.Sin(y);
+
+//Console.WriteLine(xx);
+
+//2 * pi * x
+
+//int x = 2;
+//double xx = 2 * Math.PI * x;
+//Console.WriteLine(xx);
+
+//max(x, y)
+
+//int x = 200;
+//double y = 200.52;
+
+//double xx = Math.Max(y, x);
+//Console.WriteLine(xx);
+
+//DateTime NewYear = new DateTime(2022, 12, 31, 00, 00, 00);
+//DateTime LastYear = new DateTime(2021, 12, 31, 00, 00, 00);
+//DateTime NowDay = DateTime.Now;
+//Console.WriteLine(NewYear);
+//DateTime NewYeardateOnly = NewYear.Date;
+//DateTime LastYeardateOnly = LastYear.Date;
+//DateTime NowDaydateOnly = NowDay.Date;
+
+//var x = NowDaydateOnly - LastYeardateOnly;
+//var y = NewYeardateOnly - NowDaydateOnly;
+//Console.WriteLine(x.ToString());
+//Console.WriteLine(y.ToString());
+
+
